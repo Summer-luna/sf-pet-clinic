b2f6a295f2988179d1c7c969192bd88bbf1aec3d
Xinyue (Luna) <xinyuec57@gmail.com>
2023-01-21 18:49:34 -0500

    adding base entity. closes #5

--- pet-clinic-data/src/main/java/luna/springframework/sfpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/luna/springframework/sfpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package luna.springframework.sfpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/luna/springframework/sfpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/luna/springframework/sfpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package luna.springframework.sfpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
