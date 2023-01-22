a81ccad03a7746c7ef3e975b57e21159517fe5f9
Xinyue (Luna) <xinyuec57@gmail.com>
2023-01-21 19:11:45 -0500

    refactoring to common base crud service. closes #11

--- pet-clinic-data/src/main/java/luna/springframework/sfpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/luna/springframework/sfpetclinic/services/OwnerService.java
@@ -4,13 +4,7 @@ import luna.springframework.sfpetclinic.model.Owner;
 
 import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByFirstName(String firstName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 
 }
